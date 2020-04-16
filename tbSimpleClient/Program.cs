733f3ade7fe38166f79f203f47756b7cff52a577
momek-coder <62543793+momek-coder@users.noreply.github.com>
2020-04-16 08:51:22 +0200

    Update Program.cs

--- tbSimpleClient/Program.cs
+++ tbSimpleClient/Program.cs
@@ -8,7 +8,7 @@ namespace TcpClient
     {
         private static readonly Socket ClientSocket =
             new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private const int Port = 65001;
+        private const int Port = 65002;
         
         static void Main()
         {
